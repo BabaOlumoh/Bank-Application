be570dca297e6d11907aa70c902a433194679d2f
Babatunde Olumoh <96769069+BabaOlumoh@users.noreply.github.com>
2022-11-12 12:55:31 +0100

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class BankAccount //Attributes/Data
+    {
+        private string number;
+        private double balance;
+
+        public BankAccount(string num, double bal) //Constructor 1
+        {
+            this.number = num;
+            this.balance = bal;
+        }
+        public BankAccount(string num) //overloading constructor
+        {
+            this.number = num;
+            balance = 0;
+        }
+
+        //Methods
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public bool Withdraw(double amount)
+        {
+            if (balance > amount)
+            {
+                balance -= amount;
+                return true;
+            }
+            return false;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+        public string GetNumber()
+        {
+            return number;
+        }
+        public void Close()
+        {
+            balance = 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml.Serialization;
+
+namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var accountNum1 = new BankAccount("1234567890O", 2000);
+            var accountNum2 = new BankAccount("1234567890T");
+
+            Console.WriteLine("Welcome to 419 mobile money, what would you like to do today?");
+            Console.WriteLine("insert w for withdrawal, d for deposit");
+            string choice = Console.ReadLine();
+            if (choice == "d")
+            {
+                Console.WriteLine("How much would you like to deposit please?");
+                double deposit = Convert.ToDouble(Console.ReadLine());
+                accountNum1.Deposit(deposit);
+                Console.WriteLine($"Transaction successful! your new balance {accountNum1.GetBalance()}");
+            }
+            else if (choice == "w")
+            {
+                Console.WriteLine("Kindly insert withdrawal amount");
+                double withdraw = Convert.ToInt32(Console.ReadLine());
+                bool isSuccessful = accountNum1.Withdraw(withdraw);
+
+                if (isSuccessful)
+                {
+                    Console.WriteLine($"Transaction Successful, your new balance is {accountNum1.GetBalance()}");
+                }
+                else
+                {
+                    Console.WriteLine($"Transaction Failed! your account balance is {accountNum1.GetBalance()}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid selection please try again");
+            }
+        }// main method
+    }// program class
+}//namescape class
